8567f96867d6a21184bd4511bb8b7c7bf295403b
spac-valentin <spac.valentin@gmail.com>
2015-01-06 22:48:36 +0200

    Added Person entity example

--- src/main/java/mydiaryweb/controller/HomeController.java
+++ src/main/java/mydiaryweb/controller/HomeController.java
@@ -1,7 +1,15 @@
 package mydiaryweb.controller;
 
+import mydiaryweb.models.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -9,5 +17,24 @@ public class HomeController {
     @RequestMapping(value = {"/home", "/", ""})
     public String home() {
         return "home";
+    }
+
+        @PersistenceContext(type= PersistenceContextType.TRANSACTION)
+    private EntityManager entityManager;
+
+    @RequestMapping(value="/retrieve")
+    @ResponseBody
+    public List retrieveAll() {
+        return entityManager.createQuery("SELECT e FROM Person e").getResultList();
+    }
+
+    @RequestMapping(value="/add")
+    @ResponseBody
+    @Transactional
+    public void addOne() {
+        Person person = new Person();
+        person.setEmail("added@email.com");
+        person.setName("Newly added");
+        entityManager.persist(person);
     }
 }
