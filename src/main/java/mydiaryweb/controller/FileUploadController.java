14d9d5dae0702434bb007c1c0172b5f2d84721cb
danielplecan <daniel.plecan@gmail.com>
2015-01-11 13:06:52 +0200

    File upload service implemented

--- src/main/java/mydiaryweb/controller/FileUploadController.java
+++ src/main/java/mydiaryweb/controller/FileUploadController.java
@@ -1,8 +1,14 @@
 package mydiaryweb.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import mydiaryweb.dto.file.UploadedFileDTO;
+import mydiaryweb.exception.FileUploadException;
+import mydiaryweb.service.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +21,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class FileUploadController {
+    @Autowired
+    private FileUploadService fileUploadService;
+    
     @RequestMapping(method = RequestMethod.POST, value = "/api/upload")
     @ResponseBody
     public Map<String, Object> uploadFile(@RequestBody UploadedFileDTO uploadedFileDTO) {
 
         Map<String, Object> responseMap = new HashMap<>();
         
-        responseMap.put("success", true);
+        try {
+            fileUploadService.uploadFile(uploadedFileDTO);
+            responseMap.put("success", true);
+        } catch (FileUploadException | IOException ex) {
+            Logger.getLogger(FileUploadController.class.getName()).log(Level.SEVERE, null, ex);
+            responseMap.put("success", false);
+        }
+        
 
         return responseMap;
     }
---
+++ src/main/java/mydiaryweb/exception/FileUploadException.java
@@ -0,0 +1,11 @@
+package mydiaryweb.exception;
+
+/**
+ *
+ * @author dplecan
+ */
+public class FileUploadException extends Exception{
+    public FileUploadException(String message) {
+        super(message);
+    }
+}
