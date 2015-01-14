3c8c242629cbb9aa42878bc1714b503e1c06fa48
unknown <dplecan@EN61023.endava.net>
2015-01-14 14:37:16 +0200

    Removed Person entity model

--- src/main/java/mydiaryweb/controller/HomeController.java
+++ src/main/java/mydiaryweb/controller/HomeController.java
@@ -1,15 +1,7 @@
 package mydiaryweb.controller;
 
-import mydiaryweb.models.Person;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
-import java.util.List;
 
 @Controller
 public class HomeController {
@@ -17,24 +9,5 @@ public class HomeController {
     @RequestMapping(value = {"/home", "/", ""})
     public String home() {
         return "home";
-    }
-
-        @PersistenceContext(type= PersistenceContextType.TRANSACTION)
-    private EntityManager entityManager;
-
-    @RequestMapping(value="/retrieve")
-    @ResponseBody
-    public List retrieveAll() {
-        return entityManager.createQuery("SELECT e FROM Person e").getResultList();
-    }
-
-    @RequestMapping(value="/add")
-    @ResponseBody
-    @Transactional
-    public void addOne() {
-        Person person = new Person();
-        person.setEmail("added@email.com");
-        person.setName("Newly added");
-        entityManager.persist(person);
     }
 }
