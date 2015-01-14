ca9f28a4b9f01e5d76579c431f31ac02956fecfc
unknown <dplecan@EN61023.endava.net>
2015-01-14 14:39:18 +0200

    Updated the inference service

--- src/main/java/mydiaryweb/service/InferenceService.java
+++ src/main/java/mydiaryweb/service/InferenceService.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import mydiaryweb.entity.behaviour.output.Behaviour;
 import mydiaryweb.entity.inferences.Main;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,15 @@ public class InferenceService {
         calendar.setTime(currentDate);
         
         return activities;
+    }
+    
+    public List<Main> getActivitiesByActionName(String name) {
+        List<Main> activities = new ArrayList<>();
+        
+        return activities;
+    }
+    
+    public void setBehaviourForAction(Long actionId, Behaviour newBehaviour) {
+        
     }
 }
