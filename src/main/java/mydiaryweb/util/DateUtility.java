6eed8a09886edb5e684e433f441b103d86adce42
Albert Chmilevski <albert.chmilevski@gmail.com>
2015-01-15 23:05:02 +0200

    another fix :P

--- src/main/java/mydiaryweb/util/DateUtility.java
+++ src/main/java/mydiaryweb/util/DateUtility.java
@@ -11,18 +11,20 @@ public class DateUtility {
     public static Date getBeginningOfDay(Date targetDate) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(targetDate);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        int month = calendar.get(Calendar.MONTH);
-        int year = calendar.get(Calendar.YEAR);
-        return new Date(year, month, day);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
     }
     
     public static Date getEndOfDay(Date targetDate) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(targetDate);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        int month = calendar.get(Calendar.MONTH);
-        int year = calendar.get(Calendar.YEAR);
-        return new Date(year, month, day, 23, 59, 59);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar.getTime();
     }
 }
