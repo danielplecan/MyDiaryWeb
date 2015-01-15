14a3f1285345664562fb5af6dfe3291f4d4293af
Daniel Plecan <daniel.plecan@gmail.com>
2015-01-15 21:20:05 +0200

    bla bla bla

--- src/main/java/mydiaryweb/service/MovementService.java
+++ src/main/java/mydiaryweb/service/MovementService.java
@@ -4,9 +4,14 @@
  * and open the template in the editor.
  */
 package mydiaryweb.service;
+import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import mydiaryweb.entity.inferences.Main;
 import mydiaryweb.entity.movement.output.Move;
+import mydiaryweb.util.DateUtility;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,7 +42,17 @@ public class MovementService {
         entityManager.remove(rMove);
         
         return true;
+    }
+    
+    public List<Move> getCurrentDayMoves() {
+        Date currentDate = new Date();
+        Date beginning = DateUtility.getBeginningOfDay(currentDate);
+        Date end = DateUtility.getEndOfDay(currentDate);
+        TypedQuery<Move> movesByDate = entityManager.createNamedQuery(Move.FIND_BY_DATE, Move.class);
+        movesByDate.setParameter("beginning", beginning);
+        movesByDate.setParameter("end", end);
         
+        return movesByDate.getResultList();
     }
     
 }
